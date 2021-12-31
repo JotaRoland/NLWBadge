f13b971b958035db0ee049dd18d322a8f6ce07b9
José Roland <69489127+JotaRoland@users.noreply.github.com>
2021-12-31 00:59:11 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,26 +1,22 @@
-// * set current age at project's descriptionSET PERSON'S*
 function autoAge() {
-  var today = new Date()
-  var currentMonth = today.getMonth() + 1
-  var currentYear = today.getFullYear()
-  var currentDay = today.getDay()
-  var age = 0
+  const today = new Date()
+  const currentMonth = today.getMonth() + 1
+  const currentYear = today.getFullYear()
+  const currentDay = today.getDay()
 
   function setAge(DD, MM, YYYY) {
-    age = currentYear - YYYY
-    if (currentDay <= DD && currentMonth <= MM) {
-      age--
-    }
-    return age
+    let age = currentYear - YYYY
+
+    if (currentDay <= DD && currentMonth <= MM) age--
+
+    if (age < 0) age = 'Cadê o neném?'
+
+    return `${age} years-old male (he/him). Computer Engineer student at UFMS.`
   }
 
   /*Set it to your birth day so it will change yearly.
   If you don't want your age into your bio, just comment it.*/
-  userBio.textContent = `${setAge(
-    19,
-    06,
-    2002
-  )} years-old male (he/him). Computer Engineer student at UFMS.`
+  userBio.textContent = setAge(19, 06, 2002)
 }
 
 /*Here you can link your social networks' profiles.
@@ -34,7 +30,7 @@ const userLinks = {
 function changeUserLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
-    li.children[0].href = `https://www.${social}.com/${userLinks[social]}`
+    li.children[0].children[0].href = `https://www.${social}.com/${userLinks[social]}`
   }
 }
 
